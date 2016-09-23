df2b2ee37aa9ac4913aa3f86da66f564d5ee2eda
Minh Tran <vn38minhtran@gmail.com>
2016-09-23 15:02:27 +0700

    Test

--- index.js
+++ index.js
@@ -1,13 +1,201 @@
 var postcss = require('postcss');
 
+/**
+ * Create rule name
+ * @param rule
+ * @param prefix
+ * @returns {*}
+ */
+function ruleName(rule, prefix) {
+    prefix = prefix || '';
+    return prefix + rule;
+}
+
+/**
+ * Check breakpoint value valid
+ * @param value
+ * @returns {boolean}
+ */
+function validValue(value) {
+    return !(typeof value === 'undefined' || value === null);
+}
+
+/**
+ * Check breakpoint valid
+ * @param breakpoint
+ * @param breakpoints
+ * @param requiredMax
+ * @returns {boolean}
+ */
+function validBreakpoint(breakpoint, breakpoints, requiredMax) {
+    var keys = Object.keys(breakpoints);
+    var index = keys.indexOf(breakpoint);
+    var match = breakpoints[breakpoint];
+    if (index === -1 || !match) {
+        return false;
+    }
+    requiredMax = requiredMax || false;
+    return !(requiredMax && match.indexOf(' ') === -1);
+}
+
+function throwRuleParamsError(rule, reasons) {
+    var message = 'Invalid argument of rule "@' + rule.name + '"';
+    if (reasons) {
+        message += ': ' + reasons;
+    }
+    throw rule.error(message, {
+        plugin: 'postcss-breakpoints'
+    });
+}
+
+/**
+ * Create @media rule
+ * @param min
+ * @param max
+ * @param nodes
+ * @returns {AtRule}
+ */
+function mediaRule(min, max, nodes) {
+    if (!validValue(min) && !validValue(max)) {
+        throw new Error('Invalid argument');
+    }
+    var breakpoints = [];
+    if (validValue(min)) {
+        breakpoints.push('(min-width: ' + min + ')');
+    }
+    if (validValue(max)) {
+        breakpoints.push('(max-width: ' + max + ')');
+    }
+    return postcss.atRule({
+        name: 'media',
+        params: breakpoints.join(' and '),
+        nodes: nodes
+    });
+}
+
+/**
+ * Breakpoint rule
+ * @param rule
+ */
+function breakpointRule(rule) {
+    if (!rule.params) {
+        throwRuleParamsError(rule, 'empty argument');
+    }
+    var args = rule.params.split(' ');
+    rule.replaceWith(mediaRule(args[0], args[1], rule.nodes));
+}
+
+/**
+ * Breakpoint up rule
+ * @param rule
+ * @param breakpoints
+ */
+function breakpointUpRule(rule, breakpoints) {
+    if (!rule.params) {
+        throwRuleParamsError(rule, 'empty argument');
+    }
+    if (!validBreakpoint(rule.params, breakpoints)) {
+        throwRuleParamsError(
+            rule,
+            'breakpoint "' + rule.params + '" is invalid'
+        );
+    }
+    var args = breakpoints[rule.params].split(' ');
+    rule.replaceWith(mediaRule(args[0], null, rule.nodes));
+}
+
+/**
+ * Breakpoint down rule
+ * @param rule
+ * @param breakpoints
+ */
+function breakpointDownRule(rule, breakpoints) {
+    if (!rule.params) {
+        throwRuleParamsError(rule, 'empty argument');
+    }
+    if (!validBreakpoint(rule.params, breakpoints, true)) {
+        throwRuleParamsError(
+            rule,
+            'breakpoint "' + rule.params + '" is invalid'
+        );
+    }
+    var args = breakpoints[rule.params].split(' ');
+    rule.replaceWith(mediaRule(null, args[1], rule.nodes));
+}
+
+/**
+ * Breakpoint only rule
+ * @param rule
+ * @param breakpoints
+ */
+function breakpointOnlyRule(rule, breakpoints) {
+    if (!rule.params) {
+        throwRuleParamsError(rule, 'empty argument');
+    }
+    if (!validBreakpoint(rule.params, breakpoints)) {
+        throwRuleParamsError(
+            rule,
+            'breakpoint "' + rule.params + '" is invalid'
+        );
+    }
+    var args = breakpoints[rule.params].split(' ');
+    rule.replaceWith(mediaRule(args[0], args[1], rule.nodes));
+}
+
+/**
+ * Breakpoint between rule
+ * @param rule
+ * @param breakpoints
+ */
+function breakpointBetweenRule(rule, breakpoints) {
+    if (!rule.params) {
+        throwRuleParamsError(rule, 'empty argument');
+    }
+    var names = rule.params.split(' ');
+    if (names.length !== 2) {
+        throwRuleParamsError(rule, 'invalid argument');
+    }
+    if (!validBreakpoint(names[0], breakpoints)) {
+        throwRuleParamsError(rule, 'breakpoint "' + names[0] + '" is invalid');
+    }
+    if (!validBreakpoint(names[1], breakpoints, true)) {
+        throwRuleParamsError(rule, 'breakpoint "' + names[1] + '" is invalid');
+    }
+    var argFirst = breakpoints[names[0]].split(' ');
+    var argSecond = breakpoints[names[1]].split(' ');
+    rule.replaceWith(mediaRule(argFirst[0], argSecond[1], rule.nodes));
+}
+
 module.exports = postcss.plugin('postcss-breakpoints', function (opts) {
+    var breakpoints = {
+        xs: '0 543px',
+        sm: '544px 767px',
+        md: '768px 991px',
+        lg: '992px 1199px',
+        xl: '1200px'
+    };
+
     opts = opts || {};
+    opts.breakpoints = opts.breakpoints || breakpoints;
+    opts.prefix = opts.prefix || '';
 
-    // Work with options here
-
-    return function (root, result) {
-
-        // Transform CSS AST here
-
+    return function (root) {
+        root.walkAtRules(function (rule) {
+            if (rule.name === ruleName('breakpoint', opts.prefix)) {
+                breakpointRule(rule);
+            }
+            if (rule.name === ruleName('breakpoint-up', opts.prefix)) {
+                breakpointUpRule(rule, opts.breakpoints);
+            }
+            if (rule.name === ruleName('breakpoint-down', opts.prefix)) {
+                breakpointDownRule(rule, opts.breakpoints);
+            }
+            if (rule.name === ruleName('breakpoint-only', opts.prefix)) {
+                breakpointOnlyRule(rule, opts.breakpoints);
+            }
+            if (rule.name === ruleName('breakpoint-between', opts.prefix)) {
+                breakpointBetweenRule(rule, opts.breakpoints);
+            }
+        });
     };
 });
--- test.js
+++ test.js
@@ -1,20 +1,53 @@
 import postcss from 'postcss';
-import test    from 'ava';
+import test from 'ava';
+import fs from 'fs';
 
 import plugin from './';
 
-function run(t, input, output, opts = { }) {
-    return postcss([ plugin(opts) ]).process(input)
-        .then( result => {
+function read(path) {
+    return fs.readFileSync(path, 'utf-8');
+}
+
+function run(t, name, opts = {}) {
+    var input = read('test/fixtures/' + name + '.css');
+    var output = read('test/fixtures/' + name + '.out.css');
+    return postcss([plugin(opts)])
+        .process(input)
+        .then(result => {
             t.deepEqual(result.css, output);
             t.deepEqual(result.warnings().length, 0);
         });
 }
 
-/* Write tests here
-
-test('does something', t => {
-    return run(t, 'a{ }', 'a{ }', { });
+test('breakpoint', t => {
+    return run(t, 'breakpoint', {});
 });
 
-*/
+test('breakpoint-up', t => {
+    return run(t, 'breakpoint-up', {});
+});
+
+test('breakpoint-down', t => {
+    return run(t, 'breakpoint-down', {});
+});
+
+test('breakpoint-only', t => {
+    return run(t, 'breakpoint-only', {});
+});
+
+test('breakpoint-between', t => {
+    return run(t, 'breakpoint-between', {});
+});
+
+test('custom', t => {
+    return run(t, 'custom', {
+        breakpoints: {
+            bp1: '0 599px',
+            bp2: '600px 919px',
+            bp3: '920px'
+        },
+        prefix: 'pcss-'
+    });
+});
+
+
